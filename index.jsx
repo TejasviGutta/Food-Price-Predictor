12addb24cc4574c0b6b3a3af9a1e89317eb8cf58
ashitha-g <sriashitha.g@gmail.com>
2026-05-04 16:01:41 -0400

    Update and rename index.js to index.jsx

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import "./styles.css";  // main global stylesheet
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
